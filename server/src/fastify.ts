362e32a781841608a8e2b0875690a08ba62c174a
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-04-10 21:48:20 +0100

    feat: configure fastify/jwt with the secret from .env

--- server/src/fastify.ts
+++ server/src/fastify.ts
@@ -3,7 +3,11 @@ import { configDotenv } from 'dotenv';
 
 configDotenv();
 
+if (!process.env.JWT_SECRET) throw new Error('JWT_SECRET is not defined in the environment variables');
+
 const fastify = Fastify({ logger: true });
+
 fastify.register(import('@fastify/cors'), { origin: '*' });
+fastify.register(import('@fastify/jwt'), { secret: process.env.JWT_SECRET });
 
 export default fastify;
