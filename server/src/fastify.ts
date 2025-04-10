6274683dd1dadfba6b8fcea51719194f3bbf6ada
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-04-10 21:16:18 +0100

    feat: configuring the .env file in fastify

--- server/src/fastify.ts
+++ server/src/fastify.ts
@@ -1,4 +1,7 @@
 import Fastify from 'fastify';
+import { configDotenv } from 'dotenv';
+
+configDotenv();
 
 const fastify = Fastify({ logger: true });
 fastify.register(import('@fastify/cors'), { origin: '*' });
