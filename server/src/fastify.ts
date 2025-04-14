715a6acc18e0dff395ed4a9ec5c2dfbfc55cd30e
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-04-14 21:52:39 +0100

    feat: configure fastify's core plugins (jwt, cors and cookie)

--- server/src/fastify.ts
+++ server/src/fastify.ts
@@ -5,4 +5,10 @@ configDotenv();
 
 const fastify = Fastify({ logger: true });
 
+if (!process.env.JWT_SECRET) throw new Error('JWT_SECRET is not defined in the environment variables');
+
+fastify.register(import('@fastify/jwt'), { secret: process.env.JWT_SECRET });
+fastify.register(import("@fastify/cors"), { origin: process.env.CLIENT_URL, credentials: true });
+fastify.register(import('@fastify/cookie'), { secret: process.env.COOKIE_SECRET, hook: 'onRequest', parseOptions: {} })
+
 export default fastify;
