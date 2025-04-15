f5bc126d67fff4a5f7d1c530df6f1046de4324e8
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-04-15 21:30:29 +0100

    feat: create a role and a login route for the user

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -29,6 +29,43 @@ class AuthController {
             }
         }
     }
+
+    static async signIn(request: FastifyRequest, reply: FastifyReply) {
+        const { email, password } = request.body as { email: string; password: string }
+
+        try {
+            const user = await AuthService.signIn(email, password);
+
+            reply.setCookie('authToken', user.token, {
+                path: '/',
+                httpOnly: true,
+                sameSite: 'lax',
+                secure: process.env.NODE_ENV === 'production',
+                maxAge: 60 * 60
+            });
+
+            const { token, ...userData } = user;
+            reply.code(200).send({ user: userData });
+        } catch (error) {
+            if (error instanceof AppError) {
+                reply.code(error.statusCode).send({
+                    statusCode: error.statusCode,
+                    error: error.message,
+                    message: 'Sign-in failed',
+                    status: 'fail',
+                    occurredAt: new Date().toISOString()
+                });
+            } else {
+                reply.code(500).send({
+                    statusCode: 500,
+                    error: 'Internal Server Error',
+                    message: 'An unexpected error occurred',
+                    status: 'fail',
+                    occurredAt: new Date().toISOString()
+                });
+            }
+        }
+    }
 }
 
 export default AuthController;
--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -3,4 +3,5 @@ import AuthController from "../controllers/auth.controller";
 
 export async function authRoutes(fastify: FastifyInstance) {
     fastify.post("/auth/signup", AuthController.signUp);
+    fastify.post("/auth/signin", AuthController.signIn);
 }
