d05b40dcfcc4042b1708421f63b759ec6db69abb
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-04-17 21:00:41 +0100

    feat: install and configure fastify http-proxy

--- server/src/fastify.ts
+++ server/src/fastify.ts
@@ -7,9 +7,11 @@ configDotenv();
 const fastify = Fastify({ logger: true });
 
 if (!process.env.JWT_SECRET) throw new Error('JWT_SECRET is not defined in the environment variables');
+if (!process.env.UPSTREAM_URL) throw new Error('UPSTREAM_URL is not defined in the environment variables');
 
 fastify.register(import('@fastify/jwt'), { secret: process.env.JWT_SECRET });
 fastify.register(import("@fastify/cors"), { origin: process.env.CLIENT_URL, credentials: true });
+fastify.register(import("@fastify/http-proxy"), { upstream: process.env.UPSTREAM_URL, prefix: '/pokeapi' });
 fastify.register(import('@fastify/cookie'), { secret: process.env.COOKIE_SECRET, hook: 'onRequest', parseOptions: {} });
 
 fastify.register(authRoutes);
